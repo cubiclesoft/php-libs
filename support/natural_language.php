d7b520c00e5b04affc7657c9d3aa414ba9cb6af4
cubiclesoft <webmaster@cubiclesoft.com>
2020-11-01 01:30:10 -0700

    Updated.

--- support/natural_language.php
+++ support/natural_language.php
@@ -24,8 +24,14 @@
 			if (!is_array($data))  return array("success" => false, "error" => self::NLBTranslate("Invalid data specified.  Expected array."), "errorcode" => "invalid_data");
 			if (!is_array($rules) || !isset($rules[""]))  return array("success" => false, "error" => self::NLBTranslate("Invalid rules specified.  Expected root rule."), "errorcode" => "invalid_rules");
 
+			if (!isset($options["reuse_rules"]) || $options["reuse_rules"] < 1)  $options["reuse_rules"] = 10;
+			if (!isset($options["max_depth"]) || $options["max_depth"] < 1)  $options["max_depth"] = 100;
+			if (!isset($options["max_process"]) || $options["max_process"] < 1)  $options["max_process"] = 1000;
+
 			$options["used_data"] = array();
 			$options["used_rules"] = array();
+			$options["depth"] = 1;
+			$options["processed"] = 0;
 
 			$result = self::ProcessRule($data, $rules, "", $options);
 
@@ -38,9 +44,15 @@
 		protected static function ProcessRule(&$data, &$rules, $rkey, &$options)
 		{
 			if (!isset($rules[$rkey]))  return array("success" => false, "error" => self::NLBTranslate("The rule '%s' does not exist.", $rkey), "errorcode" => "invalid_rule_reference");
-			if (isset($options["used_rules"][$rkey]))  return array("success" => false, "error" => self::NLBTranslate("Probable infinite loop detected in rules at '%s'.", $rkey), "errorcode" => "infinite_loop_detected");
 
-			$options["used_rules"][$rkey] = true;
+			if (!isset($options["used_rules"][$rkey]))  $options["used_rules"][$rkey] = 0;
+			$options["used_rules"][$rkey]++;
+
+			if ($options["used_rules"][$rkey] > $options["reuse_rules"])  return array("success" => false, "error" => self::NLBTranslate("Probable infinite loop detected in rules at '%s'.  Rule reuse limit exceeded.", $rkey), "errorcode" => "infinite_loop_detected");
+			if ($options["depth"] > $options["max_depth"])  return array("success" => false, "error" => self::NLBTranslate("Probable infinite loop detected in rules at '%s'.  Depth limit exceeded.", $rkey), "errorcode" => "max_depth_exceeded");
+
+			$options["processed"]++;
+			if ($options["processed"] > $options["max_process"])  return array("success" => false, "error" => self::NLBTranslate("Probable infinite loop detected in rules at '%s'.  Maximum number of rules processed exceeded.", $rkey), "errorcode" => "max_process_exceeded");
 
 			$rule = $rules[$rkey];
 			if (!isset($rule["type"]))  return array("success" => false, "error" => self::NLBTranslate("Rule '%s' is missing a 'type'.", $rkey), "errorcode" => "missing_rule_type");
@@ -49,7 +61,7 @@
 			{
 				// Handle data output.
 				if (!isset($rule["key"]))  return array("success" => false, "error" => self::NLBTranslate("Rule '%s' is missing a data 'key' for the 'data' type.", $rkey), "errorcode" => "missing_rule_key");
-				if (!isset($data[$rule["key"]]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist as specified by rule '%s'.", $rule["key"], $rkey), "errorcode" => "missing_data_field");
+				if (!isset($data[$rule["key"]]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist in rule '%s'.", $rule["key"], $rkey), "errorcode" => "missing_data_field");
 
 				if (!isset($options["used_data"][$rule["key"]]))  $options["used_data"][$rule["key"]] = 0;
 				$options["used_data"][$rule["key"]]++;
@@ -65,7 +77,7 @@
 					}
 					else if ($rule["format"] === "date" || $rule["format"] === "time")
 					{
-						if (!isset($rule["date"]) || !is_string($rule["date"]))  return array("success" => false, "error" => self::NLBTranslate("The 'date' format string does not exist as specified by rule '%s'.", $rkey), "errorcode" => "missing_date");
+						if (!isset($rule["date"]) || !is_string($rule["date"]))  return array("success" => false, "error" => self::NLBTranslate("The 'date' format string does not exist in rule '%s'.", $rkey), "errorcode" => "missing_date");
 
 						$ts = strtotime(($rule["format"] === "time" ? date("Y") . "-01-01 " : "") . $val);
 
@@ -76,7 +88,7 @@
 				// Apply text case changes.
 				if (isset($rule["case"]))
 				{
-					$cases = explode(",", $rule["case"]);
+					$cases = explode(",", strtolower($rule["case"]));
 					foreach ($cases as $case)
 					{
 						$case = trim($case);
@@ -103,7 +115,7 @@
 
 				$val = "";
 				$matched = 0;
-				foreach ($rule["rules"] as $rule2)
+				foreach ($rule["rules"] as $num => $rule2)
 				{
 					$valid = true;
 
@@ -130,7 +142,10 @@
 							{
 								if (substr($output, 0, 1) === "@")
 								{
+									$options["depth"]++;
 									$result = self::ProcessRule($data, $rules, substr($output, 1), $options);
+									$options["depth"]--;
+
 									if (!$result["success"])  return $result;
 
 									$val .= $result["value"];
@@ -138,7 +153,7 @@
 								else if (substr($output, 0, 2) === "[[" && substr($output, -2) === "]]")
 								{
 									$dkey = substr($output, 2, -2);
-									if (!isset($data[$dkey]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist as specified by rule '%s'.", $dkey, $rkey), "errorcode" => "missing_data_field");
+									if (!isset($data[$dkey]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist as specified by rule '%s' in the 'output' for subrule %u.", $dkey, $rkey, $num + 1), "errorcode" => "missing_data_field");
 
 									$val .= $data[$dkey];
 								}
@@ -155,6 +170,154 @@
 				}
 
 				return array("success" => true, "value" => $val);
+			}
+			else
+			{
+				return array("success" => false, "error" => self::NLBTranslate("Rule type '%s' is invalid.  Expected 'data' or 'if'.", $rule["type"]), "errorcode" => "invalid_rule_type");
+			}
+		}
+
+		public static function ValidateRules($data, &$rules, $options = array())
+		{
+			if (!is_array($data))  return array("success" => false, "error" => self::NLBTranslate("Invalid data specified.  Expected array."), "errorcode" => "invalid_data");
+			if (!is_array($rules) || !isset($rules[""]))  return array("success" => false, "error" => self::NLBTranslate("Invalid rules specified.  Expected root rule."), "errorcode" => "invalid_rules");
+
+			$numerrors = 0;
+			$options["ref_data"] = array();
+			$options["ref_rules"] = array("" => true);
+			$results = array();
+
+			$results[""] = self::ValidateRule($data, $rules, "", $options);
+			if (!$results[""]["success"])  $numerrors++;
+
+			foreach ($rules as $key => &$info)
+			{
+				if ($key !== "")
+				{
+					$results[$key] = self::ValidateRule($data, $rules, $key, $options);
+					if (!$results[$key]["success"])  $numerrors++;
+				}
+			}
+
+			$result = array(
+				"success" => true,
+				"ref_data" => $options["ref_data"],
+				"ref_rules" => $options["ref_rules"],
+				"results" => $results,
+				"num_errors" => $numerrors
+			);
+
+			return $result;
+		}
+
+		protected static function ValidateRule(&$data, &$rules, $rkey, &$options)
+		{
+			$rule = $rules[$rkey];
+			if (!isset($rule["type"]))  return array("success" => false, "error" => self::NLBTranslate("Rule '%s' is missing a 'type'.", $rkey), "errorcode" => "missing_rule_type");
+
+			if ($rule["type"] === "data")
+			{
+				// Handle data validation.
+				if (!isset($rule["key"]))  return array("success" => false, "error" => self::NLBTranslate("Rule '%s' is missing a data 'key' for the 'data' type.", $rkey), "errorcode" => "missing_rule_key");
+				if (!isset($data[$rule["key"]]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist as specified in rule '%s'.", $rule["key"], $rkey), "errorcode" => "missing_data_field");
+
+				if (!isset($options["ref_data"][$rule["key"]]))  $options["ref_data"][$rule["key"]] = 0;
+				$options["ref_data"][$rule["key"]]++;
+
+				// Check the format.
+				if (isset($rule["format"]))
+				{
+					if ($rule["format"] === "number")
+					{
+						if (isset($rule["decimals"]) && !is_int($rule["decimals"]))  return array("success" => false, "error" => self::NLBTranslate("The 'decimals' format field is not an integer value in rule '%s'.", $rkey), "errorcode" => "integer_expected");
+						if (isset($rule["decpoint"]) && !is_string($rule["decpoint"]))  return array("success" => false, "error" => self::NLBTranslate("The 'decpoint' format field is not a string value in rule '%s'.", $rkey), "errorcode" => "string_expected");
+						if (isset($rule["separator"]) && !is_string($rule["separator"]))  return array("success" => false, "error" => self::NLBTranslate("The 'separator' format field is not a string value in rule '%s'.", $rkey), "errorcode" => "string_expected");
+					}
+					else if ($rule["format"] === "date" || $rule["format"] === "time")
+					{
+						if (!isset($rule["date"]) || !is_string($rule["date"]))  return array("success" => false, "error" => self::NLBTranslate("The 'date' format field does not exist in rule '%s'.", $rkey), "errorcode" => "missing_date");
+						if (isset($rule["gmt"]) && !is_bool($rule["gmt"]))  return array("success" => false, "error" => self::NLBTranslate("The 'gmt' format field is not a boolean value in rule '%s'.", $rkey), "errorcode" => "bool_expected");
+					}
+					else
+					{
+						return array("success" => false, "error" => self::NLBTranslate("The 'format' string is not one of 'number', 'date', or 'time' in rule '%s'.", $rkey), "errorcode" => "invalid_format");
+					}
+				}
+
+				// Check the case.
+				if (isset($rule["case"]))
+				{
+					$cases = explode(",", strtolower($rule["case"]));
+					foreach ($cases as $case)
+					{
+						$case = trim($case);
+
+						if ($case !== "" && $case !== "lower" && $case !== "upper" && $case !== "words" && $case !== "first")  return array("success" => false, "error" => self::NLBTranslate("The 'case' string contains a value that is not one of 'lower', 'upper', 'words', or 'first' in rule '%s'.", $rkey), "errorcode" => "invalid_case");
+					}
+				}
+
+				// Check string replacements.
+				if (isset($rule["replace"]))
+				{
+					if (!is_array($rule["replace"]))  return array("success" => false, "error" => self::NLBTranslate("The 'replace' field is not an array in rule '%s'.", $rkey), "errorcode" => "array_expected");
+
+					foreach ($rule["replace"] as $key => $val)
+					{
+						if (!is_string($val))  return array("success" => false, "error" => self::NLBTranslate("The 'replace' values are not all strings in rule '%s'.", $rkey), "errorcode" => "string_expected");
+					}
+				}
+
+				return array("success" => true);
+			}
+			else if ($rule["type"] === "if")
+			{
+				// Handle if conditional validation.
+				if (!isset($rule["rules"]) || !is_array($rule["rules"]))  return array("success" => false, "error" => self::NLBTranslate("Rule '%s' is missing 'rules' for the 'if' type.", $rkey), "errorcode" => "missing_if_rules");
+				if (isset($rule["randomize"]) && !is_bool($rule["randomize"]))  return array("success" => false, "error" => self::NLBTranslate("The 'randomize' option is not a boolean value in rule '%s'.", $rkey), "errorcode" => "bool_expected");
+				if (isset($rule["matches"]) && !is_int($rule["matches"]))  return array("success" => false, "error" => self::NLBTranslate("The 'randomize' option is not an integer value in rule '%s'.", $rkey), "errorcode" => "bool_expected");
+
+				$matched = 0;
+				foreach ($rule["rules"] as $num => $rule2)
+				{
+					if (isset($rule2["cond"]))
+					{
+						$result = self::ParseConditional($rule2["cond"]);
+						if (!$result["success"])  return $result;
+
+						foreach ($result["tokens"] as $token)
+						{
+							if ($token[0] === "var")
+							{
+								if (!isset($data[$token[1]]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist in the conditional '%s' in rule '%s'.", $token[1], $rule2["cond"], $rkey), "errorcode" => "missing_data_field");
+
+								if (!isset($options["ref_data"][$token[1]]))  $options["ref_data"][$token[1]] = 0;
+								$options["ref_data"][$token[1]]++;
+							}
+						}
+					}
+
+					if (isset($rule2["output"]))
+					{
+						if (is_string($rule2["output"]))  $rule2["output"] = array($rule2["output"]);
+
+						foreach ($rule2["output"] as $output)
+						{
+							if (substr($output, 0, 1) === "@")
+							{
+								$rkey2 = substr($output, 1);
+								if (!isset($rules[$rkey2]))  return array("success" => false, "error" => self::NLBTranslate("The rule '%s' does not exist.", $rkey2), "errorcode" => "invalid_rule_reference");
+								else  $options["ref_rules"][$rkey2] = true;
+							}
+							else if (substr($output, 0, 2) === "[[" && substr($output, -2) === "]]")
+							{
+								$dkey = substr($output, 2, -2);
+								if (!isset($data[$dkey]))  return array("success" => false, "error" => self::NLBTranslate("The expected data field '%s' does not exist as specified by rule '%s' in the 'output' for subrule %u.", $dkey, $rkey, $num + 1), "errorcode" => "missing_data_field");
+							}
+						}
+					}
+				}
+
+				return array("success" => true);
 			}
 			else
 			{
